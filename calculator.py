4db6a152f0292bdde72fd025ab23aa7428f87726
Abdulmalik <busariabdulmalik567@gmail.com>
2026-09-12 12:11:24 +0100

    fix condition and logic

--- calculator.py
+++ calculator.py
@@ -1,16 +1,20 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 operator = input("Enter an operator (*, /, +, - ): ")
 
 if operator == "+":
     print("Result: ", num1 + num2)
+
 elif operator == "-":
     print("Result: ", num1 - num2)
+
 elif operator == "*":
     print("Result: ", num1 * num2)
+
 elif operator == "/":
-    print("Result: ", num1 / num2)
     if num2 == 0:
         print("Error: Cannot be divide by zero.")
+    else:
+        print(num1 /  num2)
 else:
     print("Error: Invalid operator")
