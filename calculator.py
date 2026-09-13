b866ce115e9b6522ad9ae3025e50ef46dd48d8b8
Abdulmalik <busariabdulmalik567@gmail.com>
2026-09-13 18:56:56 +0100

    Fix conditional statement and logic

--- calculator.py
+++ calculator.py
@@ -1,20 +1,26 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
 operator = input("Enter an operator (*, /, +, - ): ")
 
-if operator == "+":
-    print("Result: ", num1 + num2)
-
-elif operator == "-":
-    print("Result: ", num1 - num2)
-
-elif operator == "*":
-    print("Result: ", num1 * num2)
-
-elif operator == "/":
-    if num2 == 0:
-        print("Error: Cannot be divide by zero.")
-    else:
-        print(num1 /  num2)
+if  not num1.isdigit() and not num2.isdigit():
+    print("Error: Invalid Input.")
 else:
-    print("Error: Invalid operator")
+    num1 = int(num1)
+    num2 = int(num2)
+
+    if operator == "+":
+        print("Result: ", num1 + num2)
+
+    elif operator == "-":
+        print("Result: ", num1 - num2)
+
+    elif operator == "*":
+        print("Result: ", num1 * num2)
+
+    elif operator == "/":
+        if num2 == 0:
+            print("Error: Cannot be divide by zero.")
+        else:
+            print(num1 / num2)
+    else:
+        print("Error: Invalid operator")
--- times_table.py
+++ times_table.py
@@ -1,3 +1,7 @@
-number = int(input("Enter a number: "))
-for multipliers in range(1, 13):
-        print(f"{number: }*{multipliers: } = {number*multipliers: }")
+number = input("Enter a number: ")
+if not number.isdigit():
+        print("Error:Invalid Input.")
+else:
+        number = int(number)
+        for multipliers in range(1, 12):
+                print(f"{number: }*{multipliers: } = {number*multipliers: }")
